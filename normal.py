35b8b3d26ce7369d150ee1acef5d7b4f9ae866f9
gaoxin01 <87983625@qq.com>
2019-10-19 16:36:53 +0800

    Update normal.py

--- normal.py
+++ normal.py
@@ -133,7 +133,7 @@ for k in range(N):
     trainset = MyDataset(trainLabel, transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
     testset = MyDataset(testLabel, transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=True, num_workers=2)
     classes = ('cat', 'dog')
 
     # Model
