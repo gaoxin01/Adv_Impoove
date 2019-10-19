b2cebb72ef915d168c734914275523bf1e0c358f
gaoxin01 <87983625@qq.com>
2019-10-19 16:36:35 +0800

    Update Adv_Improve.py

--- Adv_Improve.py
+++ Adv_Improve.py
@@ -295,9 +295,9 @@ for k in range(N):
         trainset = MyDataset(trainLabel, transform)
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
         testset = MyDataset(testLabel, transform)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=True, num_workers=2)
         valset = MyDataset(valLabel, transform)
-        valloader = torch.utils.data.DataLoader(valset, batch_size=1, shuffle=False, num_workers=2)
+        valloader = torch.utils.data.DataLoader(valset, batch_size=1, shuffle=True, num_workers=2)
         classes = ('cat', 'dog')
 
         # Model
